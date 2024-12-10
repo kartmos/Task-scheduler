3c68f9c3c7ec2abfc912dc02e5f0a8de22829b46
kartmos <kartemgithub@gmail.com>
2024-12-11 02:55:11 +0300

    Clear version

--- task1/task1.go
+++ task1/task1.go
@@ -17,14 +17,16 @@ func timeIt(f func()) time.Duration {
 	f()
 	return time.Since(t)
 }
-func summary(input chan int, output chan int) {
+
+func reducer(input chan int, output chan int) {
+	defer close(output)
 	sum := 0
 	for item := range input {
 		sum += item
 	}
 	output <- sum
-	close(output)
 }
+
 func summarize(input []int8, output chan int, signal *sync.WaitGroup) {
 	defer signal.Done()
 	sum := 0
@@ -38,18 +40,18 @@ func generateN(nums []int8) {
 	for i := range nums {
 		nums[i] = int8(rand.Intn(127))
 	}
+	// fmt.Printf("Generate slice: %v  Len: %d\n", nums, len(nums))
 }
 
-func generator(input chan []int8, output chan []int8, inputSum chan []int8, signal *sync.WaitGroup) {
+func generator(input chan []int8, output chan []int8, signal *sync.WaitGroup) {
 	defer signal.Done()
 	slice := <-input
 	generateN(slice)
 	output <- slice
-	inputSum <- slice
 }
 
 func main() {
-	size := 1_000_000_000
+	size := 10_000
 	treads := 4
 	batch_size := size / treads
 
@@ -59,9 +61,8 @@ func main() {
 
 	inputSlice := make(chan []int8)
 	outputSlice := make(chan []int8, treads)
-	inputSum := make(chan []int8, treads)
 	outputSum := make(chan int, treads)
-	resault := make(chan int)
+	result := make(chan int)
 
 	var dur time.Duration
 
@@ -70,34 +71,26 @@ func main() {
 		defer close(inputSlice)
 		for i := 0; i < treads; i++ {
 			signalGeneration.Add(1)
-			go generator(inputSlice, outputSlice, inputSum, &signalGeneration)
+			go generator(inputSlice, outputSlice, &signalGeneration)
 		}
 		for i := 0; i < treads; i++ {
 			if i == treads-1 {
-				inputSlice <- append([]int8{}, number[cursor:size]...)
+				inputSlice <- number[cursor:size]
 			} else {
-				inputSlice <- append([]int8{}, number[cursor:cursor+batch_size]...)
+				inputSlice <- number[cursor : cursor+batch_size]
 				cursor += batch_size
 			}
 		}
 		go func() {
 			signalGeneration.Wait()
 			close(outputSlice)
-			close(inputSum)
 		}()
-		index := 0
-		for item := range outputSlice {
-			for _, val := range item {
-				number[index] = val
-				index++
-			}
-		}
 		// fmt.Printf("Biuld slice: %v  Len: %d\n", number, len(number))
 	})
 	fmt.Println("Time generation result:", dur)
 
 	dur = timeIt(func() {
-		for slice := range inputSum {
+		for slice := range outputSlice {
 			signalSummary.Add(1)
 			go summarize(slice, outputSum, &signalSummary)
 		}
@@ -106,9 +99,10 @@ func main() {
 			close(outputSum)
 		}()
 
-		go summary(outputSum, resault)
+		go reducer(outputSum, result)
 
-		sum := <-resault
+		sum := <-result
 		fmt.Println("Sum = ", sum)
 	})
+	fmt.Println("Time summarize result:", dur)
 }
