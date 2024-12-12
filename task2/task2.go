85f740afef5dc182b20b979cba6f213e1b36c0c8
kartmos <kartemgithub@gmail.com>
2024-12-12 18:02:56 +0300

    123

--- task2/task2.go
+++ task2/task2.go
@@ -20,8 +20,8 @@ var batchSize int
 func init() {
 	flag.IntVar(&threadsSum, "s", 1, "Count treads Sum")
 	flag.IntVar(&threadsGen, "g", 1, "Count treads Gen")
-	flag.IntVar(&batchSize, "b", 1, "Slice size")
-	flag.IntVar(&size, "sN", 1, "Batch Size")
+	flag.IntVar(&batchSize, "bs", 1, "Batch Size")
+	flag.IntVar(&size, "n", 1, "Size")
 }
 
 func reducer(input chan int, output chan int) {
@@ -33,7 +33,7 @@ func reducer(input chan int, output chan int) {
 	output <- sum
 }
 
-func reduce(input chan int) <-chan int {
+func Reduce(input chan int) <-chan int {
 	oneShot := make(chan int)
 	go reducer(input, oneShot)
 	return oneShot
@@ -46,13 +46,6 @@ func summary(slice []int8) (sum int) {
 	return
 }
 
-func summarize(input chan []int8, output chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for slice := range input {
-		output <- summary(slice)
-	}
-}
-
 func generateN(slice []int8) []int8 {
 	for i := range slice {
 		slice[i] = int8(rand.Intn(127))
@@ -60,33 +53,8 @@ func generateN(slice []int8) []int8 {
 	return slice
 }
 
-func generator(input chan []int8, output chan []int8, signal *sync.WaitGroup) {
-	defer signal.Done()
-	for slice := range input {
-		output <- generateN(slice)
-	}
-}
-
-func guard(wg *sync.WaitGroup, closeChan any) {
-	wg.Wait()
-
-	switch closeChan := closeChan.(type) {
-	case chan []int8:
-		close(closeChan)
-	case chan int:
-		close(closeChan)
-	default:
-		panic("wtf don't what to do")
-	}
-}
-
-// Generic guard
-func gg[T chan int | chan []int8](wg *sync.WaitGroup, closeChan T) {
-	wg.Wait()
-	close(closeChan)
-}
-
 func pusher(v []int8, bs int) (output chan []int8) {
+	output = make(chan []int8)
 	size := len(v)
 	go func() {
 		defer close(output)
@@ -104,10 +72,11 @@ func work[I any, O any](input chan I, output chan O, wg *sync.WaitGroup, execute
 	}
 }
 
-func pool[I any, O any](input chan I, n int, execute func(msg I) O) (output chan O) {
+func Map[I any, O any](input chan I, n int, execute func(msg I) O) (output chan O) {
+	output = make(chan O)
 	wg := &sync.WaitGroup{}
+	wg.Add(n)
 	for i := 0; i < n; i++ {
-		wg.Add(1)
 		go work(input, output, wg, execute)
 	}
 
@@ -122,31 +91,13 @@ func pool[I any, O any](input chan I, n int, execute func(msg I) O) (output chan
 func main() {
 	flag.Parse()
 
-	var wgGen sync.WaitGroup
-	var wgSum sync.WaitGroup
-
 	numbers := make([]int8, size)
 
-	input := pusher(numbers, batchSize)
-	bridge := make(chan []int8)
+	values := pusher(numbers, batchSize)
+	bridge := Map(values, threadsGen, generateN)
+	result := Map(bridge, threadsSum, summary)
 
-	for i := 0; i < threadsGen; i++ {
-		wgGen.Add(1)
-		go generator(input, bridge, &wgGen)
-	}
-
-	go gg(&wgGen, bridge)
-
-	output := make(chan int)
-
-	for i := 0; i < threadsSum; i++ {
-		wgSum.Add(1)
-		go summarize(bridge, output, &wgSum)
-	}
-
-	go gg(&wgSum, output)
-
-	sum := <-reduce(output)
+	sum := <-Reduce(result)
 
 	fmt.Println("Result sum: ", sum)
 	fmt.Println("Len: ", len(numbers))
